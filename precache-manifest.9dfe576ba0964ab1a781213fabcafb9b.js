ae0970a84b1c929e1eb6cb50a2fb7b967293594f
Sebastian Waisbrot <seppo0010@gmail.com>
2021-11-08 13:47:16 -0300

    Updates

---
+++ precache-manifest.9dfe576ba0964ab1a781213fabcafb9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5fed88824f74d3839499a2ff96ce3d5",
+    "url": "/sysarmy-sueldos-2021.2/index.html"
+  },
+  {
+    "revision": "5bffd430cef1847cf10a",
+    "url": "/sysarmy-sueldos-2021.2/static/css/main.095e5f73.chunk.css"
+  },
+  {
+    "revision": "dbb30239cbfbc09f9cd9",
+    "url": "/sysarmy-sueldos-2021.2/static/js/2.691b23c2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/sysarmy-sueldos-2021.2/static/js/2.691b23c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bffd430cef1847cf10a",
+    "url": "/sysarmy-sueldos-2021.2/static/js/main.de1d57b0.chunk.js"
+  },
+  {
+    "revision": "5671925becfa6f7f9ed5",
+    "url": "/sysarmy-sueldos-2021.2/static/js/runtime-main.86da5444.js"
+  }
+]);
